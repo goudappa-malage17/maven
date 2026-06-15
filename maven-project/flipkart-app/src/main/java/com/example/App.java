46c3d8a7190ef4fb085c7d975aece024425192f0
Goudappa Malage <goudappamalage10@gmail.com>
2026-06-15 19:22:03 +0530

    Add files via upload

---
+++ maven-project/flipkart-app/src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Sum = " + (a + b));
+        System.out.println("Application executed successfully");
+    }
+}
